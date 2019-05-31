8ca752bc8935686f4bf222b671f2c8d62435eeb9
yanhao <yanhao@feiyuai.com>
2019-05-31 15:00:30 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
-</style>
+<script>
+import { CometChat } from "@cometchat-pro/chat";
+import "./App.css";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.initializeApp();
+  },
+  methods: {
+    initializeApp() {
+      var appID = '379556fe47c979';
+      CometChat.init(appID).then(
+        () => {
+          console.log("Initialization completed successfully");
+        },
+        error => {
+          console.log("Initialization failed with error:", error);
+        }
+      );
+    }
+  }
+};
+</script>
